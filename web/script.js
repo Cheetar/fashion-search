6edf76277416df84f4d0941b679f2a1531f4436d
krzys-h <krzys_h@interia.pl>
2020-07-13 10:52:31 +0200

    File upload finally working!

--- web/script.js
+++ web/script.js
@@ -24,8 +24,47 @@ $('#cancelButton, #foundAgain').click(function() {
 $('#searchButton').click(function() {
     $('#uploadConfirm').css('display', 'none');
     $('#uploadProgress').css('display', '');
-    setTimeout(function() {
-        $('#foundImage').attr('src', $('#imageResult').attr('src'));
+
+    $('.progress-bar').width('100%');
+    $('.progress-bar').addClass('.progress-bar-animated');
+
+    var formData = new FormData($('#form')[0]);
+    formData.append('file', $('input[name=file]')[0].files[0]); // wtf?
+
+    $.ajax({
+        url: "/",
+        type: 'POST',
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false,
+        uploadProgress: function(evt) {
+            if (evt.lengthComputable) {
+                var percentComplete = evt.loaded / evt.total;
+                var percentValue = percentComplete + '%';
+                $("#progressBar").animate({
+                    width: percentValue
+                }, {
+                    duration: 500,
+                    easing: "linear"
+                });
+                if (percentComplete == 100)
+                    $('.progress-bar').addClass('.progress-bar-animated');
+                else
+                    $('.progress-bar').removeClass('.progress-bar-animated');
+            } else {
+                $('.progress-bar').addClass('.progress-bar-animated');
+            }
+        }
+    }).done(function(data) {
+        $('#foundImage').attr('src', data.found_image);
+        $('#foundBuy').attr('href', data.url);
+        $('#foundPrice').text(data.price);
         $('#imageCardContainer').addClass('card-container-manualflip');
-    }, 3000);
+    }).fail(function() {
+        $('#uploadConfirm').css('display', '');
+        $('#uploadProgress').css('display', 'none');
+        $('.progress-bar').addClass('.progress-bar-animated');
+        alert('Oops something went wrong.');
+    });
 });

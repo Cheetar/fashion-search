7b91e4e03ad845e77dd1b2224a00e0868eae928f
Piotr Maksymiuk <2003piotr@gmail.com>
2020-07-13 12:12:36 +0200

    ai fixes

--- ai/app.py
+++ ai/app.py
@@ -8,20 +8,15 @@ from keras.applications.vgg16 import VGG16, preprocess_input
 from flask import Flask
 
 app = Flask(__name__)
-
-
-def main():
-    image.LOAD_TRUNCATED_IMAGES = True
-    model = VGG16(weights='imagenet', include_top=False)
-    db = open_connection()
-    
+ 
 
 def open_connection():
     username = os.environ["MARIADB_USERNAME"]
     password = os.environ["MARIADB_PASSWORD"]
     host = "db"
-    database = password = os.environ["MARIADB_DATABASE"]
-    return pymysql.connect(host, username, password, database)
+    database = os.environ["MARIADB_DATABASE"]
+    cursorclass = pymysql.cursors.DictCursor
+    return pymysql.connect(host, username, password, database, cursorclass = pymysql.cursors.DictCursor)
 
 def toList(string):
     string = string.strip('[]')
@@ -33,34 +28,34 @@ def distance(vector1, vector2):
     return sum(map(lambda x, y: (x-y)**2, vector1, vector2))
     
 def calculate(imgpath):
-    try:    
-        img = image.load_img('/img/' + imgpath, target_size=(224, 224))
-        img_data = image.img_to_array(img)
-        img_data = np.expand_dims(img_data, axis=0)
-        img_data = preprocess_input(img_data)
-        features = np.array(model.predict(img_data))
-        serialized_data = features.flatten().tolist()
-        return str(serialized_data)
-    except:
-        raise Exception('Calculating failed')
+    #try:    
+    img = image.load_img('/img/' + imgpath, target_size=(224, 224))
+    img_data = image.img_to_array(img)
+    img_data = np.expand_dims(img_data, axis=0)
+    img_data = preprocess_input(img_data)
+    features = np.array(model.predict(img_data))
+    serialized_data = features.flatten().tolist()
+    return str(serialized_data)
+    #except:
+    #    raise Exception('Calculating failed')
 
 
 @app.route('/add/<id>')    
 def add(id):
-    try:
-        with db.cursor() as cursor:
-            sql = "SELECT `image_path` FROM `images` WHERE `id`=%s"
-            cursor.execute(sql, (id))
-            result = cursor.fetchone()
-
-            vector = calculate('/img/' + result.image_path)
-            sql = "INSERT INTO `images` (`vector`) VALUES (%s)"
-            cursor.execute(sql, (vector))
-            print('Added ID:',id)
-            os.remove('/img/' + result.image_path)
-            return 'ok', 200       
-    except:
-        return 'error', 400
+    #try:
+    with db.cursor() as cursor:
+        sql = "SELECT `image_path` FROM `images` WHERE `id`=%s"
+        cursor.execute(sql, (id))
+        result = cursor.fetchone()
+        print(str(result['image_path']))
+        vector = calculate('/img/' + str(result['image_path']))
+        sql = "INSERT INTO `images` (`vector`) VALUES (%s)"
+        cursor.execute(sql, (vector))
+        print('Added ID:',id)
+        os.remove('/img/' + str(result['image_path']))
+        return 'ok', 200       
+    #except:
+    #    return 'error', 400
 
 @app.route('/find/<user_img>')
 def findBest(user_img):
@@ -78,7 +73,7 @@ def findBest(user_img):
                 dst = distance(user_vector.vector, result.vector)
                 distances.append({'id':result.id, 'distance':dst})
             
-            best_distance = 10**25, best_id = -1            
+            best_distance, best_id = 10**25, -1            
             for i in distances:
                 if i.distance < bestDistance:
                     best_id, best_distance = i.id, i.distance
@@ -100,8 +95,9 @@ def test():
             return 'error', 400
 
 
-if __name__ == "__main__":
-    main()
+image.LOAD_TRUNCATED_IMAGES = True
+model = VGG16(weights='imagenet', include_top=False)
+db = open_connection()
 
 '''
 image.LOAD_TRUNCATED_IMAGES = True
